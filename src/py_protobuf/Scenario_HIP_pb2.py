2fe3554d6f2779cf68fe8e7c1e79246180dac7ca
KRolander <kromesrolandgyula@gmail.com>
2025-06-05 11:21:10 +0200

    Add In/Out Standing train conversion to the create_HIP_scenario_from_Source() function

--- src/py_protobuf/Scenario_HIP_pb2.py
+++ src/py_protobuf/Scenario_HIP_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   syntax='proto3',
   serialized_options=None,
   create_key=_descriptor._internal_create_key,
-  serialized_pb=b'\n\x12Scenario_HIP.proto\x12\rmypackage_hip\x1a\x12Location_HIP.proto\"Z\n\x08Scenario\x12%\n\x02in\x18\x01 \x01(\x0b\x32\x19.mypackage_hip.ScenarioIn\x12\'\n\x03out\x18\x02 \x01(\x0b\x32\x1a.mypackage_hip.ScenarioOut\":\n\nScenarioIn\x12,\n\x06trains\x18\x01 \x03(\x0b\x32\x1c.mypackage_hip.IncomingTrain\"A\n\x0bScenarioOut\x12\x32\n\rtrainRequests\x18\x01 \x03(\x0b\x32\x1b.mypackage_hip.TrainRequest\"\xa9\x01\n\rIncomingTrain\x12\x16\n\x0e\x65ntryTrackPart\x18\x01 \x01(\x04\x12\x1d\n\x15\x66irstParkingTrackPart\x18\x06 \x01(\x04\x12\x0f\n\x07\x61rrival\x18\x02 \x01(\x04\x12\x11\n\tdeparture\x18\x03 \x01(\x04\x12\n\n\x02id\x18\x04 \x01(\t\x12\x31\n\x07members\x18\x05 \x03(\x0b\x32 .mypackage_hip.IncomingTrainUnit\"h\n\x11IncomingTrainUnit\x12+\n\ttrainUnit\x18\x01 \x01(\x0b\x32\x18.mypackage_hip.TrainUnit\x12&\n\x05tasks\x18\x02 \x03(\x0b\x32\x17.mypackage_hip.TaskSpec\"\xab\x01\n\x0cTrainRequest\x12\x16\n\x0eleaveTrackPart\x18\x01 \x01(\x04\x12\x1c\n\x14lastParkingTrackPart\x18\x06 \x01(\x04\x12\x0f\n\x07\x61rrival\x18\x02 \x01(\x04\x12\x11\n\tdeparture\x18\x03 \x01(\x04\x12\x13\n\x0b\x64isplayName\x18\x04 \x01(\t\x12,\n\ntrainUnits\x18\x05 \x03(\x0b\x32\x18.mypackage_hip.TrainUnit\"C\n\tTrainUnit\x12\n\n\x02id\x18\x01 \x01(\t\x12*\n\x04type\x18\x02 \x01(\x0b\x32\x1c.mypackage_hip.TrainUnitType\"\xc1\x01\n\rTrainUnitType\x12\x13\n\x0b\x64isplayName\x18\x01 \x01(\t\x12\x11\n\tcarriages\x18\x02 \x01(\r\x12\x0e\n\x06length\x18\x04 \x01(\x01\x12\x18\n\x10reversalDuration\x18\x03 \x01(\x04\x12\x17\n\x0f\x63ombineDuration\x18\x05 \x01(\x04\x12\x15\n\rsplitDuration\x18\x06 \x01(\x04\x12\x14\n\x0c\x62\x61\x63kNormTime\x18\x07 \x01(\x04\x12\x18\n\x10\x62\x61\x63kAdditionTime\x18\x08 \x01(\x04\"j\n\x0cShuntingUnit\x12\n\n\x02id\x18\x01 \x01(\t\x12)\n\x07members\x18\x02 \x03(\x0b\x32\x18.mypackage_hip.TrainUnit\x12\x11\n\tparentIDs\x18\x03 \x03(\t\x12\x10\n\x08\x63hildIDs\x18\x04 \x03(\t\"Y\n\x08TaskSpec\x12%\n\x04type\x18\x01 \x01(\x0b\x32\x17.mypackage_hip.TaskType\x12\x14\n\x08priority\x18\x02 \x01(\rB\x02\x18\x01\x12\x10\n\x08\x64uration\x18\x03 \x01(\x04\x62\x06proto3'
+  serialized_pb=b'\n\x12Scenario_HIP.proto\x12\rmypackage_hip\x1a\x12Location_HIP.proto\"\xee\x01\n\x08Scenario\x12%\n\x02in\x18\x01 \x01(\x0b\x32\x19.mypackage_hip.ScenarioIn\x12\'\n\x03out\x18\x02 \x01(\x0b\x32\x1a.mypackage_hip.ScenarioOut\x12\x35\n\ninStanding\x18\x03 \x01(\x0b\x32!.mypackage_hip.ScenarioInStanding\x12\x37\n\x0boutStanding\x18\x04 \x01(\x0b\x32\".mypackage_hip.ScenarioOutStanding\x12\x11\n\tstartTime\x18\x05 \x01(\x04\x12\x0f\n\x07\x65ndTime\x18\x06 \x01(\x04\":\n\nScenarioIn\x12,\n\x06trains\x18\x01 \x03(\x0b\x32\x1c.mypackage_hip.IncomingTrain\"A\n\x0bScenarioOut\x12\x32\n\rtrainRequests\x18\x01 \x03(\x0b\x32\x1b.mypackage_hip.TrainRequest\"B\n\x12ScenarioInStanding\x12,\n\x06trains\x18\x01 \x03(\x0b\x32\x1c.mypackage_hip.IncomingTrain\"I\n\x13ScenarioOutStanding\x12\x32\n\rtrainRequests\x18\x01 \x03(\x0b\x32\x1b.mypackage_hip.TrainRequest\"\xa9\x01\n\rIncomingTrain\x12\x16\n\x0e\x65ntryTrackPart\x18\x01 \x01(\x04\x12\x1d\n\x15\x66irstParkingTrackPart\x18\x06 \x01(\x04\x12\x0f\n\x07\x61rrival\x18\x02 \x01(\x04\x12\x11\n\tdeparture\x18\x03 \x01(\x04\x12\n\n\x02id\x18\x04 \x01(\t\x12\x31\n\x07members\x18\x05 \x03(\x0b\x32 .mypackage_hip.IncomingTrainUnit\"h\n\x11IncomingTrainUnit\x12+\n\ttrainUnit\x18\x01 \x01(\x0b\x32\x18.mypackage_hip.TrainUnit\x12&\n\x05tasks\x18\x02 \x03(\x0b\x32\x17.mypackage_hip.TaskSpec\"\xc2\x01\n\x0cTrainRequest\x12\x16\n\x0eleaveTrackPart\x18\x01 \x01(\x04\x12\x1c\n\x14lastParkingTrackPart\x18\x06 \x01(\x04\x12\x0f\n\x07\x61rrival\x18\x02 \x01(\x04\x12\x11\n\tdeparture\x18\x03 \x01(\x04\x12\x13\n\x0b\x64isplayName\x18\x04 \x01(\t\x12,\n\ntrainUnits\x18\x05 \x03(\x0b\x32\x18.mypackage_hip.TrainUnit\x12\x15\n\rstandingIndex\x18\x07 \x01(\x01\"C\n\tTrainUnit\x12\n\n\x02id\x18\x01 \x01(\t\x12*\n\x04type\x18\x02 \x01(\x0b\x32\x1c.mypackage_hip.TrainUnitType\"\xc1\x01\n\rTrainUnitType\x12\x13\n\x0b\x64isplayName\x18\x01 \x01(\t\x12\x11\n\tcarriages\x18\x02 \x01(\r\x12\x0e\n\x06length\x18\x04 \x01(\x01\x12\x18\n\x10reversalDuration\x18\x03 \x01(\x04\x12\x17\n\x0f\x63ombineDuration\x18\x05 \x01(\x04\x12\x15\n\rsplitDuration\x18\x06 \x01(\x04\x12\x14\n\x0c\x62\x61\x63kNormTime\x18\x07 \x01(\x04\x12\x18\n\x10\x62\x61\x63kAdditionTime\x18\x08 \x01(\x04\"j\n\x0cShuntingUnit\x12\n\n\x02id\x18\x01 \x01(\t\x12)\n\x07members\x18\x02 \x03(\x0b\x32\x18.mypackage_hip.TrainUnit\x12\x11\n\tparentIDs\x18\x03 \x03(\t\x12\x10\n\x08\x63hildIDs\x18\x04 \x03(\t\"Y\n\x08TaskSpec\x12%\n\x04type\x18\x01 \x01(\x0b\x32\x17.mypackage_hip.TaskType\x12\x14\n\x08priority\x18\x02 \x01(\rB\x02\x18\x01\x12\x10\n\x08\x64uration\x18\x03 \x01(\x04\x62\x06proto3'
   ,
   dependencies=[Location__HIP__pb2.DESCRIPTOR,])
 
@@ -49,6 +49,34 @@ _SCENARIO = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='inStanding', full_name='mypackage_hip.Scenario.inStanding', index=2,
+      number=3, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='outStanding', full_name='mypackage_hip.Scenario.outStanding', index=3,
+      number=4, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='startTime', full_name='mypackage_hip.Scenario.startTime', index=4,
+      number=5, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='endTime', full_name='mypackage_hip.Scenario.endTime', index=5,
+      number=6, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -61,8 +89,8 @@ _SCENARIO = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=57,
-  serialized_end=147,
+  serialized_start=58,
+  serialized_end=296,
 )
 
 
@@ -93,8 +121,8 @@ _SCENARIOIN = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=149,
-  serialized_end=207,
+  serialized_start=298,
+  serialized_end=356,
 )
 
 
@@ -125,8 +153,72 @@ _SCENARIOOUT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=209,
-  serialized_end=274,
+  serialized_start=358,
+  serialized_end=423,
+)
+
+
+_SCENARIOINSTANDING = _descriptor.Descriptor(
+  name='ScenarioInStanding',
+  full_name='mypackage_hip.ScenarioInStanding',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='trains', full_name='mypackage_hip.ScenarioInStanding.trains', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=425,
+  serialized_end=491,
+)
+
+
+_SCENARIOOUTSTANDING = _descriptor.Descriptor(
+  name='ScenarioOutStanding',
+  full_name='mypackage_hip.ScenarioOutStanding',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  create_key=_descriptor._internal_create_key,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='trainRequests', full_name='mypackage_hip.ScenarioOutStanding.trainRequests', index=0,
+      number=1, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto3',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=493,
+  serialized_end=566,
 )
 
 
@@ -192,8 +284,8 @@ _INCOMINGTRAIN = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=277,
-  serialized_end=446,
+  serialized_start=569,
+  serialized_end=738,
 )
 
 
@@ -231,8 +323,8 @@ _INCOMINGTRAINUNIT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=448,
-  serialized_end=552,
+  serialized_start=740,
+  serialized_end=844,
 )
 
 
@@ -286,6 +378,13 @@ _TRAINREQUEST = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
+    _descriptor.FieldDescriptor(
+      name='standingIndex', full_name='mypackage_hip.TrainRequest.standingIndex', index=6,
+      number=7, type=1, cpp_type=5, label=1,
+      has_default_value=False, default_value=float(0),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR,  create_key=_descriptor._internal_create_key),
   ],
   extensions=[
   ],
@@ -298,8 +397,8 @@ _TRAINREQUEST = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=555,
-  serialized_end=726,
+  serialized_start=847,
+  serialized_end=1041,
 )
 
 
@@ -337,8 +436,8 @@ _TRAINUNIT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=728,
-  serialized_end=795,
+  serialized_start=1043,
+  serialized_end=1110,
 )
 
 
@@ -418,8 +517,8 @@ _TRAINUNITTYPE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=798,
-  serialized_end=991,
+  serialized_start=1113,
+  serialized_end=1306,
 )
 
 
@@ -471,8 +570,8 @@ _SHUNTINGUNIT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=993,
-  serialized_end=1099,
+  serialized_start=1308,
+  serialized_end=1414,
 )
 
 
@@ -517,14 +616,18 @@ _TASKSPEC = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1101,
-  serialized_end=1190,
+  serialized_start=1416,
+  serialized_end=1505,
 )
 
 _SCENARIO.fields_by_name['in'].message_type = _SCENARIOIN
 _SCENARIO.fields_by_name['out'].message_type = _SCENARIOOUT
+_SCENARIO.fields_by_name['inStanding'].message_type = _SCENARIOINSTANDING
+_SCENARIO.fields_by_name['outStanding'].message_type = _SCENARIOOUTSTANDING
 _SCENARIOIN.fields_by_name['trains'].message_type = _INCOMINGTRAIN
 _SCENARIOOUT.fields_by_name['trainRequests'].message_type = _TRAINREQUEST
+_SCENARIOINSTANDING.fields_by_name['trains'].message_type = _INCOMINGTRAIN
+_SCENARIOOUTSTANDING.fields_by_name['trainRequests'].message_type = _TRAINREQUEST
 _INCOMINGTRAIN.fields_by_name['members'].message_type = _INCOMINGTRAINUNIT
 _INCOMINGTRAINUNIT.fields_by_name['trainUnit'].message_type = _TRAINUNIT
 _INCOMINGTRAINUNIT.fields_by_name['tasks'].message_type = _TASKSPEC
@@ -535,6 +638,8 @@ _TASKSPEC.fields_by_name['type'].message_type = Location__HIP__pb2._TASKTYPE
 DESCRIPTOR.message_types_by_name['Scenario'] = _SCENARIO
 DESCRIPTOR.message_types_by_name['ScenarioIn'] = _SCENARIOIN
 DESCRIPTOR.message_types_by_name['ScenarioOut'] = _SCENARIOOUT
+DESCRIPTOR.message_types_by_name['ScenarioInStanding'] = _SCENARIOINSTANDING
+DESCRIPTOR.message_types_by_name['ScenarioOutStanding'] = _SCENARIOOUTSTANDING
 DESCRIPTOR.message_types_by_name['IncomingTrain'] = _INCOMINGTRAIN
 DESCRIPTOR.message_types_by_name['IncomingTrainUnit'] = _INCOMINGTRAINUNIT
 DESCRIPTOR.message_types_by_name['TrainRequest'] = _TRAINREQUEST
@@ -564,6 +669,20 @@ ScenarioOut = _reflection.GeneratedProtocolMessageType('ScenarioOut', (_message.
   # @@protoc_insertion_point(class_scope:mypackage_hip.ScenarioOut)
   })
 _sym_db.RegisterMessage(ScenarioOut)
+
+ScenarioInStanding = _reflection.GeneratedProtocolMessageType('ScenarioInStanding', (_message.Message,), {
+  'DESCRIPTOR' : _SCENARIOINSTANDING,
+  '__module__' : 'Scenario_HIP_pb2'
+  # @@protoc_insertion_point(class_scope:mypackage_hip.ScenarioInStanding)
+  })
+_sym_db.RegisterMessage(ScenarioInStanding)
+
+ScenarioOutStanding = _reflection.GeneratedProtocolMessageType('ScenarioOutStanding', (_message.Message,), {
+  'DESCRIPTOR' : _SCENARIOOUTSTANDING,
+  '__module__' : 'Scenario_HIP_pb2'
+  # @@protoc_insertion_point(class_scope:mypackage_hip.ScenarioOutStanding)
+  })
+_sym_db.RegisterMessage(ScenarioOutStanding)
 
 IncomingTrain = _reflection.GeneratedProtocolMessageType('IncomingTrain', (_message.Message,), {
   'DESCRIPTOR' : _INCOMINGTRAIN,
--- src/scenario_generator/scenario_generator.py
+++ src/scenario_generator/scenario_generator.py
@@ -127,6 +127,10 @@ class ScenarioGenerator:
         print("LOG: | call create_HIP_scenario()|")
         incomingTrains = getattr(self.scenario_hip, "in")
         
+        self.scenario_hip.startTime = self.scenario.startTime
+        self.scenario_hip.endTime = self.scenario.endTime
+        
+        
         _in = getattr(self.scenario, "in")
         
         for train_standard in _in:
@@ -162,8 +166,47 @@ class ScenarioGenerator:
                         train_member.trainUnit.type.splitDuration = trainUnitType.splitDuration
                         train_member.trainUnit.type.backNormTime = trainUnitType.backNormTime
                         train_member.trainUnit.type.backAdditionTime = trainUnitType.backAdditionTime
-                  
-                                 
+                       
+        
+        
+        inStandningTrains = getattr(self.scenario_hip, "inStanding")
+        
+        _inStandingTrains = getattr(self.scenario, "inStanding")
+        
+        for train_standard in _inStandingTrains:
+            train = inStandningTrains.trains.add()
+            train.entryTrackPart = train_standard.sideTrackPart
+            train.firstParkingTrackPart = train_standard.parkingTrackPart
+            train.arrival = train_standard.time
+            train.departure = train_standard.time
+            train.id = train_standard.id
+            
+            members_standard = train_standard.members
+            for member in members_standard:
+                train_member = train.members.add()
+                train_member.trainUnit.id = member.id
+                
+                for task_standard in member.tasks:
+                    task = train_member.tasks.add()
+                    if task_standard.type.predefined:
+                        task.type.predefined = task_standard.type.predefined
+                    elif task_standard.type.other:
+                        task.type.other = task_standard.type.other
+                   
+                    task.priority = task_standard.priority
+                    task.duration = task_standard.duration
+                
+                #Get typeDisplayName associated to the typeDisplayName
+                for trainUnitType in self.scenario.trainUnitTypes:
+                    if trainUnitType.displayName == member.typeDisplayName:
+                        train_member.trainUnit.type.displayName = trainUnitType.typePrefix          
+                        train_member.trainUnit.type.carriages = trainUnitType.carriages
+                        train_member.trainUnit.type.length = trainUnitType.length
+                        train_member.trainUnit.type.combineDuration = trainUnitType.combineDuration
+                        train_member.trainUnit.type.splitDuration = trainUnitType.splitDuration
+                        train_member.trainUnit.type.backNormTime = trainUnitType.backNormTime
+                        train_member.trainUnit.type.backAdditionTime = trainUnitType.backAdditionTime
+                        
             
         trainRequest = getattr(self.scenario_hip, "out")
         _out = getattr(self.scenario, "out")
@@ -181,7 +224,37 @@ class ScenarioGenerator:
             
             
             members_standard = train_standard.members
-            for member in members_standard:
+            for member in members_standard:                
+                train_unit = train.trainUnits.add()
+                # train_unit.id = member.id -> it is "***" in cTORS in HIP it is simply undefined
+                 
+                for trainUnitType in self.scenario.trainUnitTypes:
+                    if trainUnitType.displayName == member.typeDisplayName:
+                        train_unit.type.displayName = trainUnitType.typePrefix          
+                        train_unit.type.carriages = trainUnitType.carriages
+                        train_unit.type.length = trainUnitType.length
+                        train_unit.type.combineDuration = trainUnitType.combineDuration
+                        train_unit.type.splitDuration = trainUnitType.splitDuration
+                        train_unit.type.backNormTime = trainUnitType.backNormTime
+                        train_unit.type.backAdditionTime = trainUnitType.backAdditionTime
+        
+       
+        trainRequest = getattr(self.scenario_hip, "outStanding")
+                
+        for train_standard in self.scenario.outStanding:
+            train = trainRequest.trainRequests.add()
+            train.leaveTrackPart = train_standard.sideTrackPart
+            train.lastParkingTrackPart = train_standard.parkingTrackPart
+            
+            train.arrival = train_standard.time
+            train.departure = train_standard.time
+            train.displayName = train_standard.id
+            
+            ## Add train units 
+            
+            
+            members_standard = train_standard.members
+            for member in members_standard:                
                 train_unit = train.trainUnits.add()
                 # train_unit.id = member.id -> it is "***" in cTORS in HIP it is simply undefined
                  
@@ -195,35 +268,6 @@ class ScenarioGenerator:
                         train_unit.type.backNormTime = trainUnitType.backNormTime
                         train_unit.type.backAdditionTime = trainUnitType.backAdditionTime
                 
-                    
-            # for train_standard in self.scenario.outStanding:
-            #     train = trainRequest.trainRequests.add()
-            #     train.leaveTrackPart = train_standard.sideTrackPart
-            #     train.lastParkingTrackPart = train_standard.parkingTrackPart
-                
-            #     train.arrival = train_standard.time
-            #     train.departure = train_standard.time
-            #     train.displayName = train_standard.id
-                
-            #     ## Add train units 
-                
-                
-            #     members_standard = train_standard.members
-            #     for member in members_standard:
-            #         train_unit = train.trainUnits.add()
-            #         # train_unit.id = member.id -> it is "***" in cTORS in HIP it is simply undefined
-                     
-            #         for trainUnitType in self.scenario_TrainUnitTypes:
-            #             if trainUnitType.displayName == member.typeDisplayName:
-            #                 train_unit.type.displayName = trainUnitType.displayName          
-            #                 train_unit.type.carriages = trainUnitType.carriages
-            #                 train_unit.type.length = trainUnitType.length
-            #                 train_unit.type.combineDuration = trainUnitType.combineDuration
-            #                 train_unit.type.splitDuration = trainUnitType.splitDuration
-            #                 train_unit.type.backNormTime = trainUnitType.backNormTime
-            #                 train_unit.type.backAdditionTime = trainUnitType.backAdditionTime
-                
-
         print(MessageToJson(self.scenario_hip, including_default_value_fields=True))
         
     # Add outgoing train to the scenario
@@ -717,7 +761,7 @@ class ScenarioGeneratorHIP(ScenarioGenerator):
     def __init__(self, standardScenarioGenerator: ScenarioGenerator):
         super().__init__()
         self.scenario_hip = standardScenarioGenerator.scenario_hip
-    
+            
     # Converts protobuf object into json representation and saves it into .json file 
     def save_scenario_json(self, file_name: str):
         print("LOG: | call save_scenario_json()|")
