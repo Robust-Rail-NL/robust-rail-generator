213d8b159f98cd9b8eceed8b368d4de9c001b108
Léon Planken <l.r.planken@tudelft.nl>
2026-06-24 13:53:28 +0200

    Add regression baseline
    These json files were generated from the old, protobuf-based code by
    running `src/generate-scenarios.sh` and can be used to check whether the
    output after migration to Pydantic corresponds (closely enough) to the
    previous format.

---
+++ src/generate-scenarios.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 main.py -c scenario_config_example1.json
+python3 main.py -c scenario_config_example2.json
+python3 main.py -c scenario_config_example3.json
+python3 main.py -c scenario_config_random_distribution1.json
+python3 main.py -c scenario_config_random_distribution2.json
+python3 main.py -c scenario_config_test.json
